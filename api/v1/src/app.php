8949bd5654119b6de72f078c513726da8894c58f
Thomas Galinis <tgalinis2020@fau.edu>
2021-03-08 22:27:17 -0500

    enable container cache

--- api/v1/src/app.php
+++ api/v1/src/app.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$builder = new \DI\ContainerBuilder();
+$builder = new DI\ContainerBuilder();
 
 // Bootstrap the container.
 $builder->addDefinitions(__DIR__ . '/def/settings.php');
@@ -15,9 +15,9 @@ $builder->addDefinitions(__DIR__ . '/def/services.php');
 // Uncomment if definition cache is available.
 // The app doesn't have write permissions on FAU's LAMP server outside
 // of public_html so the cache cannot be created on-the-fly.
-//$container->enableCompilation(__DIR__ . '/../var/cache');
+$container->enableCompilation(__DIR__ . '/../var/cache');
 
-$app = new \Slim\App($builder->build());
+$app = new Slim\App($builder->build());
 
 (require __DIR__ . '/middleware.php')($app);
 (require __DIR__ . '/routes.php')($app);
