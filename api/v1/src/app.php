d80ed09194116d9459067c5d4e2966d0d37c9b7f
Thomas Galinis <tgalinis2020@fau.edu>
2021-03-08 22:27:54 -0500

    rename container to builder in app.php

--- api/v1/src/app.php
+++ api/v1/src/app.php
@@ -15,7 +15,7 @@ $builder->addDefinitions(__DIR__ . '/def/services.php');
 // Uncomment if definition cache is available.
 // The app doesn't have write permissions on FAU's LAMP server outside
 // of public_html so the cache cannot be created on-the-fly.
-$container->enableCompilation(__DIR__ . '/../var/cache');
+$builder->enableCompilation(__DIR__ . '/../var/cache');
 
 $app = new Slim\App($builder->build());
 
