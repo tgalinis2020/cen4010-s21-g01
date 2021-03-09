5577fc3b688358e3bc4daf9e6a299b5a9875bc07
Thomas Galinis <tgalinis2020@fau.edu>
2021-03-08 22:32:54 -0500

    comment out container cache option
    put app bootstrapping code in an immediately invoked closure

--- api/v1/src/app.php
+++ api/v1/src/app.php
@@ -4,22 +4,26 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$builder = new DI\ContainerBuilder();
+return (function () {
+    $builder = new DI\ContainerBuilder();
 
-// Bootstrap the container.
-$builder->addDefinitions(__DIR__ . '/def/settings.php');
-$builder->addDefinitions(__DIR__ . '/def/handlers.php');
-$builder->addDefinitions(__DIR__ . '/def/middleware.php');
-$builder->addDefinitions(__DIR__ . '/def/services.php');
+    // Bootstrap the container.
+    $builder->addDefinitions(__DIR__ . '/def/settings.php');
+    $builder->addDefinitions(__DIR__ . '/def/handlers.php');
+    $builder->addDefinitions(__DIR__ . '/def/middleware.php');
+    $builder->addDefinitions(__DIR__ . '/def/services.php');
 
-// Uncomment if definition cache is available.
-// The app doesn't have write permissions on FAU's LAMP server outside
-// of public_html so the cache cannot be created on-the-fly.
-$builder->enableCompilation(__DIR__ . '/../var/cache');
+    // Uncomment if definition cache is available.
+    // The app doesn't have write permissions on FAU's LAMP server outside
+    // of public_html so the cache cannot be created on-the-fly.
+    // It must be generated in another machine and pushed into the git repo.
+    //$builder->enableCompilation(__DIR__ . '/../var/cache');
 
-$app = new Slim\App($builder->build());
+    $app = new Slim\App($builder->build());
 
-(require __DIR__ . '/middleware.php')($app);
-(require __DIR__ . '/routes.php')($app);
+    (require __DIR__ . '/middleware.php')($app);
+    (require __DIR__ . '/routes.php')($app);
 
-return $app;
+    return $app;
+})();
+
