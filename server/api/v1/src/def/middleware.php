458b213e0e1d5772c8fa971acbf54e78877725ac
Thomas Galinis <tgalinis2020@fau.edu>
2021-03-11 17:51:32 -0500

    rename database
    fix SessionMiddleware dependencies

--- server/api/v1/src/def/middleware.php
+++ server/api/v1/src/def/middleware.php
@@ -11,11 +11,8 @@ use function DI\factory;
 return [
     
     SessionMiddleware::class => factory(function (ContainerInterface $c) {
-        $settings = $c->get('settings')['firebase']['php-jwt'];
-
         return new SessionMiddleware(
-            $settings['secret_key'],
-            $settings['allowed_algs']
+            $c->get('jwt_decoder')
         );
     }),
 
