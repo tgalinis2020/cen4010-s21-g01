6aa4bdbc5681439595313283dd8cede0ec1e8910
Thomas Galinis <tgalinis2020@fau.edu>
2021-04-03 01:36:31 -0400

    prototype: fix upload URL and rebuild app

--- vertical-prototype/src/api/upload_image.js
+++ vertical-prototype/src/api/upload_image.js
@@ -1,3 +1,6 @@
 export default function upload_image(file) {
-    return fetch(`https://lamp.cse.fau.edu/~cen4010_s21_g01/uploads`, { method: 'POST', body: file })
+    return fetch(
+        `https://lamp.cse.fau.edu/~cen4010_s21_g01/api-v1.php/upload`,
+        { method: 'POST', body: file }
+    )
 }
