55fb835802c0a9ba7f7a22fe8e5eb18d60a04a1b
Thomas Galinis <tgalinis2020@fau.edu>
2021-04-02 22:48:27 -0400

    prototype: use JSON.parse to encode request body
    rebuild prototype

--- vertical-prototype/src/api/api_request.js
+++ vertical-prototype/src/api/api_request.js
@@ -1,5 +1,5 @@
 export default function api_request(method, target, payload = null) {
-    const url = `https://lamp.cse.fau.edu/~cen4010_s21_g01/api-v1.php/${target}`
+    const url = `https://lamp.cse.fau.edu/~cen4010_s21_g01/api-v1.php${target}`
     const init = {
         method,
         headers: {
@@ -9,7 +9,7 @@ export default function api_request(method, target, payload = null) {
     }
 
     if (['POST', 'PUT', 'PATCH'].includes(method)) {
-        init['body'] = payload
+        init['body'] = JSON.stringify(payload)
     }
 
     return fetch(url, init).then(r => r.json())
