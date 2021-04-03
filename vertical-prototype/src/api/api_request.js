71a369bc5b3bf6eeb3d738494f0b98106eb05fdb
Thomas Galinis <tgalinis2020@fau.edu>
2021-04-02 22:22:59 -0400

    prototype: add bootstrap stylesheet
    prototype: omit body parameter when making GET requests in api_request function

--- vertical-prototype/src/api/api_request.js
+++ vertical-prototype/src/api/api_request.js
@@ -1,10 +1,16 @@
 export default function api_request(method, target, payload = null) {
-    return fetch(`https://lamp.cse.fau.edu/~cen4010_s21_g01${target}`, {
+    const url = `https://lamp.cse.fau.edu/~cen4010_s21_g01${target}`
+    const init = {
         method,
         headers: {
             'Content-Type': 'application/json',
             'Accept':       'application/json',
-        },
-        body: JSON.stringify({ data: payload }),
-    }).then(r => r.json())
+        }
+    }
+
+    if (['POST', 'PUT', 'PATCH'].includes(method)) {
+        init['body'] = payload
+    }
+
+    return fetch(url, init).then(r => r.json())
 }
